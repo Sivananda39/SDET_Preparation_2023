9712ef8c828aad4eed2defa6857ea178f7afd9ea
Sivananda39 <65295548+Sivananda39@users.noreply.github.com>
2023-05-02 18:01:18 +0530

    Added for Week1 Day 1 day2
    Added files as of now, upcoming programs will upload via gitbash and will maintain folder structure

---
+++ week1day2program3_RemoveOccurenceinanArray.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+import org.testng.annotations.Test;
+
+public class week1day2program3_RemoveOccurenceinanArray {
+//	3. Remove the first and last occurrence of a given number in the input array
+//	input = {1,2,3,4,5,1}, number = 1
+//	output = {2,3,5}
+			 
+	
+	
+
+	@Test
+	public void example1() {
+		
+		int a[] = { 1, 2, 3, 4, 5, 1 };
+		int target = 1;
+		int result[] = removeOccurence(a, target);
+		System.out.println("Pos case :");
+		System.out.println(Arrays.toString(result));
+		removeOccurence(a, target );
+	}
+	
+	
+
+
+	public static int[] removeOccurence(int[] a, int target ) {
+		int occNum = 0;
+		for (int temp : a) {
+			if (temp == target)
+				occNum++;
+		}
+		int out[] = new int[a.length - occNum];
+		int index=0;
+		if(occNum == 0) return a;
+		for(int temp1 : a) {
+			if(temp1 != target) {
+				out[index++] = temp1;
+			}
+		}
+		return out;
+		 
+	}
+
+	
+}
