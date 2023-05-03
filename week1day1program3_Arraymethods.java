405dd265e823ec4d087980300208a5920139a257
Sivananda39 <65295548+Sivananda39@users.noreply.github.com>
2023-05-03 06:32:25 +0530

    updated methodname

--- week1day1program3_Arraymethods.java
+++ week1day1program3_Arraymethods.java
@@ -70,7 +70,7 @@ public void example1() {
 
 		int[] nums= {1,2,3,4,5};
 		int remove = 4;
-		 for (int i = 0; i < nums.length-2; i++) {
+		 for (int i = 0; i < nums.length; i++) {
 			 if (nums[i] != remove) {
 				 System.out.println(nums[i]);
 			 }
