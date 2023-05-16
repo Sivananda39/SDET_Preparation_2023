cb6ecc37f8196bc2b88e8e7e58bad492e83e72fa
Sivananda39 <65295548+Sivananda39@users.noreply.github.com>
2023-05-16 10:34:47 +0530

    Week3Day1

---
+++ week3day1program2_maximumAVGSubArray.java
@@ -0,0 +1,51 @@
+package week3_WeekdayProblems;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class week3day1program2_maximumAVGSubArray {
+//	 Homework - 08/05/2023
+//	 https://leetcode.com/problems/maximum-average-subarray-i
+///You are given an integer array nums consisting of n elements, and an integer k.
+//
+//Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+//
+// 
+//
+//Example 1:
+//
+//Input: nums = [1,12,-5,-6,50,3], k = 4
+//Output: 12.75000
+//Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		maxAvgSubArray();
+	}
+
+	public static void maxAvgSubArray() {
+		int[] nums = {1,12,-5,-6,50,3};
+		int k=4;
+		int sum=0;
+		double maxOutputAvg = 0, temp=0;
+		for (int i = 0; i < k; i++) {
+			sum=sum + nums[i];
+		}
+		System.out.println(sum);
+		maxOutputAvg = sum/k;
+		temp=maxOutputAvg;
+		for (int j = k; j < nums.length; j++) {
+			sum = sum + nums[j] - nums[j - k];
+			maxOutputAvg = sum / k;
+			if (temp > maxOutputAvg)
+				maxOutputAvg = temp;
+			temp = maxOutputAvg;
+		}
+		System.out.println(maxOutputAvg);
+		
+		if (temp < maxOutputAvg)
+			System.out.println(maxOutputAvg);
+	}
+
+}
