c93f7bf9475f5232736a9885a4606c36f2238519
Sivananda39 <65295548+Sivananda39@users.noreply.github.com>
2023-06-15 15:38:26 +0530

    added weekdayHW

---
+++ week4day5program2_crawLogFolder.java
@@ -0,0 +1,25 @@
+package week4_WeekdayProblems;
+
+import java.util.ArrayList;
+import java.util.*;
+
+import org.junit.Test;
+
+public class week4day5program2_crawLogFolder {
+
+//		Homework 19/05/2023:
+//	https://leetcode.com/problems/crawler-log-folder/
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		logFolder();
+	
+	}
+
+	public static void logFolder() {
+
+	}
+
+
+}
